e076f00c74cd6f8604a782dd0b5f69550e2943b9
zhanghan22 <zhanghan22@kuaishou.com>
2025-12-15 14:55:59 +0800

    修改base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
-	base: "/",
+	site: "https://lakers2.github.io",
+	base: "/test-github-page",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
